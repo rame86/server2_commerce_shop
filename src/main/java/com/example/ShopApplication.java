e5a68cd3e66a284cc8a79cd9e1ab735910b4a30f
thdud00 <suyeong1526@daum.net>
2026-03-03 10:45:07 +0900

    3.3 첫커밋

--- src/main/java/com/example/ShopApplication.java
+++ src/main/java/com/example/ShopApplication.java
@@ -1,8 +1,8 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // @SpringBootApplication
 // public class ShopApplication {
@@ -14,7 +14,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 // }
 
 // ShopApplication.java
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // DB 자동설정 제외
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // DB 자동설정 제외
+// public class ShopApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(ShopApplication.class, args);
+//     }
+// }
+@SpringBootApplication
+@MapperScan("com.example.shop.repository") // ShopMapper가 있는 패키지 경로를 지정
 public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
