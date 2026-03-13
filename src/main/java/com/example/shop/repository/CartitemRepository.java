652a1c9ed8fef60002b2266532850d89f92b9725
thdud00 <suyeong1526@daum.net>
2026-03-13 10:49:30 +0900

    z

--- src/main/java/com/example/shop/repository/CartitemRepository.java
+++ src/main/java/com/example/shop/repository/CartitemRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.shop.entity.CartItem;
 
-public interface CartitemRepository extends JpaRepository<CartItem, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findByCart_CartIdAndProduct_ProductId(Long cartId, Long productId);
 }
