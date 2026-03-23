9d7867aa6f61cd9d5af7bf32d0dd38704f7798e3
thdud00 <suyeong1526@daum.net>
2026-03-23 17:42:13 +0900

    .

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class ProductController {
 
--- src/main/java/com/example/shop/controller/ShopController.java
+++ src/main/java/com/example/shop/controller/ShopController.java
@@ -26,7 +26,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/shop")
+@RequestMapping("/")
 @Slf4j
 @RequiredArgsConstructor
 public class ShopController {
